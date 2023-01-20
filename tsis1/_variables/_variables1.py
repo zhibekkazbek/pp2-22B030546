003a5f5367f7f8381f5acaa43d81af3538604a2e
zhibek <zhibekk04@gmail.com>
2023-01-20 12:53:39 +0600

    file

---
+++ tsis1/_variables/_variables1.py
@@ -0,0 +1,15 @@
+x = 5   # x is of type int
+y = "John"  # y is of type str
+print(x)
+print(y)
+
+# we can specify the data type of a variable
+x = str(3) # x will be a digit 3
+y = int(3) # y will be a integer number 3
+z = float(3) # z will be a 3.0
+
+# or we can get the data type of a variable with the type() function
+x = 5
+y = "John"
+print(type(x)) # output will be : <class 'int'>
+print(type(y)) # output will be : <class 'str'>
