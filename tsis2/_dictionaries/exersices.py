eb6f4fe0dc153b53db2a7d3a26bd4d91bb8cc1c5
zhibek <zhibekk04@gmail.com>
2023-01-26 17:18:39 +0600

    changed

---
+++ tsis2/_dictionaries/exersices.py
@@ -0,0 +1,35 @@
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))
+#_____________________
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"] = 2020
+#_____________________
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["color"] = "red"
+#_____________________
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+#_____________________
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
+#_____________________
