688deb44ed3e5cd9456c5b1eb3a337c1654c608b
zhibek <zhibekk04@gmail.com>
2023-03-27 23:10:50 +0600

    file

--- tsis6/ex.py
+++
@@ -1,8 +0,0 @@
-import os
-path = r"C:\Users\Сырым\Documents\pp2-22B030546\tsis6"
-
-print(os.getcwd())
-print(os.listdir('.'))
-print(os.listdir(os.getcwd()))
-os.chdir('./tests')
-print(os.getcwd())
